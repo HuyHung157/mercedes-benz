ab393692140301e7a350975dacde1647a19ee55d
HuyHung157 <hhungtran157@gmail.com>
2020-02-13 12:22:00 +0700

    commiting changes

---
+++ js/scroll-reveal.js
@@ -0,0 +1,35 @@
+const sr = ScrollReveal({
+    duration: 2000,
+    reset: true
+});
+sr.reveal('.slider',{
+    delay: 20,
+});
+sr.reveal('.brandhub-search',{
+    delay: 20,
+});
+sr.reveal('.headlinecopy',{
+    delay: 100,
+});
+sr.reveal('.channeltitle',{
+});
+sr.reveal('.branhub__btn-group',{
+    delay: 20,
+});
+sr.reveal('.brandhub',{
+    delay: 20,
+});
+sr.reveal('.showmore__container',{
+});
+sr.reveal('.footnotes',{
+    delay: 20,
+});
+sr.reveal('.sitemap',{
+    delay: 20,
+});
+sr.reveal('.footerinfor',{
+    delay: 20,
+});
+sr.reveal('.footerlogo',{
+    delay: 20,
+});
